c7ad22709581f62b6b72497d6aab217175b093ad
Pawel b <46341827+pawel9009@users.noreply.github.com>
2022-03-02 16:07:08 +0100

    Add files via upload

---
+++ cw2.py
@@ -0,0 +1,69 @@
+# zad1 = int(input())
+# print(f'{zad1} {zad1*2} {zad1*3} {zad1*4}')
+
+# zad2 = int(input())
+# print(zad2*24*3600)
+
+#
+# print(0.1 + (0.1 + 0.1))
+# print(0.9 + (0.2 + 0.1))
+#
+# print(0.2 * (0.2 + 0.1))
+# print((0.9 * 0.1) * 0.3)
+
+# dane = input().split()
+# l1, l2, l3, l4 = float(dane[0]), float(dane[1]), float(dane[2]), float(dane[3])
+# suma = l1 + l2 + l3 + l4
+# srednia = suma/4
+# print(srednia)
+# wariancja = ((l1-srednia)**2+ (l2-srednia)**2 +(l3-srednia)**2 +(l4-srednia)**2)/4
+# print(wariancja)
+
+# dane = input().split()
+# x1, y1, x2, y2 = float(dane[0]), float(dane[1]), float(dane[2]), float(dane[3])
+# odleglosc = ((x1 - x2)**2 + (y1 - y2)**2)**0.5
+# print(odleglosc)
+
+
+# a, b, c = 10, 4, 5
+# if a > 3 or b < 1:
+#     print("1")
+# if a != 0 or b > 14:
+#     print("2")
+# if c == 5 or a == 1:
+#     print("3")
+#
+# if a >= 0 and b < 4:
+#     print("4")
+# if b < 40 and c >= 11:
+#     print("5")
+# if a == 34 and c <= 1:
+#     print("6")
+# if a > 1 and c >= 1:
+#     print("7")
+#
+# if not a > 10:
+#     print('8')
+# if not b == a:
+#     print('9')
+# if not c > 1:
+#     print('10')
+
+# x, y, z = 65, 23, 51
+#
+# if z > 20:
+#     print("1")
+# if x == 65:
+#     print("2")
+# if z == y:
+#     print("3")
+# if x != 0:
+#     print("4")
+# if y < 0 and y == z:
+#     print("5")
+#
+# if z == y or z > x or x < z:
+#     print("4")
+# if x != z or x < y or x >= y:
+#     print("5")
+
