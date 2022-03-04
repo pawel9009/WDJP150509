5862925fa5903c965bd92de5d433839a218d1e1b
Pawel b <46341827+pawel9009@users.noreply.github.com>
2022-03-04 19:25:03 +0100

    Update cw2.py

--- cw2.py
+++ cw2.py
@@ -1,30 +1,29 @@
 # zad1 = int(input())
-# print(f'{zad1} {zad1*2} {zad1*3} {zad1*4}')
-
+# print(f'{zad1} {zad1 * 2} {zad1 * 3} {zad1 * 4}')
+#
 # zad2 = int(input())
-# print(zad2*24*3600)
-
+# print(zad2 * 24 * 3600)
 #
 # print(0.1 + (0.1 + 0.1))
 # print(0.9 + (0.2 + 0.1))
-#
 # print(0.2 * (0.2 + 0.1))
 # print((0.9 * 0.1) * 0.3)
-
+#
+# zad4
 # dane = input().split()
 # l1, l2, l3, l4 = float(dane[0]), float(dane[1]), float(dane[2]), float(dane[3])
 # suma = l1 + l2 + l3 + l4
-# srednia = suma/4
-# print(srednia)
-# wariancja = ((l1-srednia)**2+ (l2-srednia)**2 +(l3-srednia)**2 +(l4-srednia)**2)/4
+# srednia = suma / 4
+# wariancja = ((l1 - srednia) ** 2 + (l2 - srednia) ** 2 + (l3 - srednia) ** 2 + (l4 - srednia) ** 2) / 4
 # print(wariancja)
-
+#
+# zad5
 # dane = input().split()
 # x1, y1, x2, y2 = float(dane[0]), float(dane[1]), float(dane[2]), float(dane[3])
-# odleglosc = ((x1 - x2)**2 + (y1 - y2)**2)**0.5
+# odleglosc = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 # print(odleglosc)
-
-
+#
+# zad6
 # a, b, c = 10, 4, 5
 # if a > 3 or b < 1:
 #     print("1")
@@ -32,7 +31,6 @@
 #     print("2")
 # if c == 5 or a == 1:
 #     print("3")
-#
 # if a >= 0 and b < 4:
 #     print("4")
 # if b < 40 and c >= 11:
@@ -41,16 +39,15 @@
 #     print("6")
 # if a > 1 and c >= 1:
 #     print("7")
-#
 # if not a > 10:
 #     print('8')
 # if not b == a:
 #     print('9')
 # if not c > 1:
 #     print('10')
-
-# x, y, z = 65, 23, 51
 #
+# zad 7
+# x, y, z = 65, 23, 51
 # if z > 20:
 #     print("1")
 # if x == 65:
@@ -61,9 +58,37 @@
 #     print("4")
 # if y < 0 and y == z:
 #     print("5")
-#
+# if x == 65 and y == 23 and z == 51:
+#     print("6")
+# if 0 == 0:
+#     print("7")
+# if z:
+#     print("8")
 # if z == y or z > x or x < z:
-#     print("4")
+#     print("9")
 # if x != z or x < y or x >= y:
-#     print("5")
+#     print("10")
+
+# zad 8
+# x = 5
+# y = 8
+# print(x * y)
+# print(x / y)
+# print(x // y)
+# print(x % y)
+# print(-x % y)
+# print(abs(y))
+# print(float(x))
+# print(complex(x, y))
+# print(divmod(x, y))
+# print(pow(x, y))
+# print(x ** y)
+
+# zad9
+# x = 2
+# y = 4
+# zad = [x >> 2, y << 2, x | y, y ^ x, x & y, ~x, x ^ y >> x, x >> 3 ^ y << 4, y << x, y | 2]
+# for x in zad:
+#     print(x)
+
 
